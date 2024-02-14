6cb2821ae3423b483ac627384cdf44fd95ec0928
Roshan <roshanblone100@gmail.com>
2024-02-14 02:47:23 +0000

    postman config

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,9 +2,9 @@ import {v2 as cloudinary} from 'cloudinary';
 import fs from 'fs'
 
 cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key:process.env.CLOUDINARY_API_KEY, 
+    api_secret:process.env.CLOUDINARY_API_SECRET
   });
 
   const uploadOnCloudinary = async (localFilePath) =>{
@@ -15,7 +15,10 @@ cloudinary.config({
             resource_type: "auto"
         })
         // File has been uploaded successfully
-        console.log("file is uploaded on cloudinary", response.url);
+        // console.log("file is uploaded on cloudinary", response.url);
+
+        fs.unlinkSync(localFilePath)
+
         return response
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the uploaded operation got failed
